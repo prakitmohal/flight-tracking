c5c8a10d612ee663e671ed1f5d9d7a7f5b9a15e6
Prakit Mohal <prakitmohal@gmail.com>
2021-11-11 08:55:50 -0500

    added proof of concept that finds a plane within parameters and pushes it to Slack. This completes the Proof of Concept next steps are to run this in the background to tweak parameters

--- flight-tracker.py
+++ flight-tracker.py
@@ -1,10 +1,42 @@
-import os, requests, json, time, geopy.distance
+import os, requests, json, time, geopy.distance, sys, requests, base64
 
+# slack test
+def slackTest(link,message):
+    
+    title = "PLANE DETECTED :airplane:"
+    slackData = {
+ 
+        "username": "Testing",
+        "attachments": [
+            {
+                "color": "#FF0000",
+                "fields": [
+                    {
+                        "title": title,
+                        "value": message,
+                        "short": "false",
+ 
+                    }
+                ]
+            }
+        ]
+    }
+     
+    # Size of the slack data
+    byte_length = str(sys.getsizeof(slackData))
+    headers = {'Content-Type': "application/json",
+               'Content-Length': byte_length}
+     
+    # Posting requests after dumping the slack data
+    response = requests.post(link, data=json.dumps(slackData), headers=headers)
+
+ 
 # read config file
 config = open("config.txt","r")
 link = config.readline().strip()
 lat = config.readline()
 lon = config.readline()
+slackLink = config.readline().strip()
 myCoords = (lat,lon)
 config.close()
 
@@ -41,9 +73,10 @@ while(1):
         for x in range(entries):
             print (flightData[x])
    
-        # if a plane is within 2nm and under 10,000 feet this is special
+        # if a plane is within 2nm and under 10,000 feet we can hear it
         if flightData[0][1] < 2 and flightData[0][2] < 10000:
             print ("Can you hear this plane: ",flightData[0][0])
-         
+            slackTest(slackLink,flightData[0][0])
+    
     time.sleep(15)
 
